ea2d3c84f6c03f454322ca5dad75560c4da7435f
Prashantk20 <69709335+Prashantk20@users.noreply.github.com>
2020-12-07 19:47:33 +0530

    Add files via upload

---
+++ AssigningTeams.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for left in range(7):
+    for right in range(left,7):
+        print("["+ str(left) + "|" + str(right) + "]", end=" ")
+    print()
+
+
+# In[ ]:
+
+
+
+
